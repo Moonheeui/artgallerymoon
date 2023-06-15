134473f92eb8f271f4797fc2bc33b1a87601ca5a
Moonheeui <lovekmhrosa@gmail.com>
2023-06-15 17:26:12 -0400

    Update files

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final Project - Moonhee Kim</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@800;900&family=Poltawski+Nowy:wght@400;500;600&family=Poppins:wght@300;400&family=Roboto&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <header class="main-header">
+        <div class="container">
+            <div class="header-flex">
+                <a href="#" class="logo"><img src="../images/logo.png" alt="Art Gallery Logo.png"></a>
+                <a href="#" class="bar"><i class="fa-solid fa-bars"></i></a>
+                <nav>
+                    <ul class="nav-list">
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Artists</a>
+                        </li>
+                        <li>
+                            <a href="">Services</a>
+                        </li>
+                        <li>
+                            <a href="">Exhibitions</a>
+                        </li>
+                        <li>
+                            <a href="">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+                <a href="#" class="signin"><i class="fa-solid fa-right-to-bracket"></i></a>
+            </div>
+        </div>
+    </header>
+
+    <section class="big-banner">
+        <div class="textbox">
+            <h3>we create a valuable world</h3>
+            <h1>art gallery <br> moon</h1>
+            <a href="" class="banner-btn"><i class="fa-solid fa-circle-play"></i>watch video</a>
+        </div>
+    </section>
+
+    <section class="about">
+        <div class="container">
+            <div class="about-flex">
+                <div class="left">
+                    <h4>about us</h4>
+                    <div class="divider"></div>
+                    <h2>if there are no artists, there is no gallery</h2>
+                    <p>On June 17, 2000, Art Gallery Moon took the very first steps of its journey in Montreal. For the three years that followed, we opened exhibitions for countless artists in her role as an art dealer, galvanizing their drive to create while serving as a builder of social bridges connecting these artists with the enthusiasts. We will continue to pour all our care and energy into supporting artists and shining a light on their achievements. Our heartfelt thanks goes out to all those who have supported us, their noble eyes and hearts ever committed to the search for beauty.</p>
+                    <a href="" class="about-btn">more information</a>
+                </div>
+                <div class="right"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="products">
+        <div class="container">
+            <h2>on view</h2>
+            <div class="products-flex">
+                <div class="left">
+                    <div class="image image1">
+                        <div class="grey-box">
+                            <h3>void in fullness</h3>
+                            <p>Minjung Kim</p>
+                        </div>
+                    </div>
+                    <div class="image image2">
+                        <div class="grey-box">
+                            <h3>together together</h3>
+                            <p>Jinhan Lee</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="image image3">
+                        <div class="grey-box">
+                            <h3>flowers in the vase</h3>
+                            <p>Sungyoon Kim</p>
+                        </div>
+                    </div>
+                    <div class="image image4">
+                        <div class="grey-box">
+                            <h3>frieze new york</h3>
+                            <p>Geuntaek Yoo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="" class="products-btn">load more</a>
+        </div>
+    </section>
+
+    <section class="services">
+        <div class="container">
+            <div class="services-flex">
+                <div class="left">
+                    <h4>featured services</h4>
+                    <div class="divider"></div>
+                    <h2>the services that we are providing</h2>
+                    <p>We leverage our custom art studio teams so our artists observe client requests firsthand, eliminating confusion or communication delays. During these collaborative processes our art consultants and project managers work closely with clients to save time and optimize productivity.</p>
+                </div>
+
+                <div class="right">
+                    <div class="right-container">
+                        <div class="card-flex">
+                            <div class="card">
+                                <a href="" class="icon-link"><i class="fa-solid fa-window-maximize"></i></a>
+
+                                <h3>Featured Exhibition</h3>
+                                <p>The exhibition represents artists whose deep connection to nature serves as their primary source of inspiration.</p>
+                            </div>
+                            <div class="card">
+                                <a href="" class="icon-link"><i class="fa-solid fa-calendar"></i></i></a>
+                                <h3>Upcoming Events</h3>
+                                <p>Art Gallery Moon is pleased to present an all-women group exhibition, featuring a selection of works by Daniela Busarello.</p>
+                            </div>
+                            <div class="card">
+                                <a href="" class="icon-link"><i class="fa-solid fa-chalkboard-user"></i></i></a>
+                                <h3>Learning & Schools</h3>
+                                <p>With our state of the art studios, there is no limit to what you can create with us.</p>
+                            </div>
+                            <div class="card">
+                                <a href="" class="icon-link"><i class="fa-solid fa-palette"></i></i></a>
+                                <h3>Collection</h3>
+                                <p>From fine art original artworks to an extensive image library with limitless print options, we have what you are looking for.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="events">
+        <div class="container">
+            <h2>upcoming exhibitions</h2>
+
+            <div class="events-flex">
+                <div class="card" data-aos="flip-right" data-aos-duration="1000">
+                    <div class="image image1"></div>
+                    <div class="info">
+                        <h3>Frieze New York 2023 </h3>
+                        <p>July 10 – July 15, 2023</p>
+                    </div>
+                </div>
+                <div class="card" data-aos="flip-right" data-aos-duration="1000" data-aos-delay="500">
+                    <div class="image image2"></div>
+                    <div class="info">
+                        <h3>Frieze New York 2023 </h3>
+                        <p>July 16 – July 20, 2023</p>
+                    </div>
+                </div>
+
+                <div class="card" data-aos="flip-right" data-aos-duration="1000" data-aos-delay="1000">
+                    <div class="image image3"></div>
+                    <div class="info">
+                        <h3>Frieze LA 2023 </h3>
+                        <p>August 10 – August 15, 2023</p>
+                    </div>
+                </div>
+            </div>
+
+            <a href="" class="events-btn">book online</a>
+        </div>
+
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="cta-flex">
+                <div class="card">
+                    <div class="info1">
+                        <h3>contact info</h3>
+                        <p>Address : <br> 3797 Westmount, <br> Montreal, Quebec, Canada <br><br> By appointment only : <br> info@artgallarymoon.com <br> +1 514 675 6615</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="info2">
+                        <h3>open info</h3>
+                        <p>Tuesday to Saturday <br> 10 am - 5 pm <br> * Last Admission - 4 : 30 pm</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="social-media-list">
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                        <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <hr><br>
+            <h4>© 2023 Moonhee Kim</h4>
+        </div>
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 100) {
+                $(".main-header").addClass("has-bg");
+            } else {
+                $(".main-header").removeClass("has-bg");
+            }
+        });
+    </script>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script>
+        AOS.init();
+    </script>
+
+</body>
+
+</html>
